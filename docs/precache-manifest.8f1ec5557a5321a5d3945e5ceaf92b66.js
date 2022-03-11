28d9f9fe63660c93abb23bc3e7f0e4267ed4f285
Jude Wood <jude.wood@salecycle.com>
2022-03-11 13:31:28 +0000

    I just committed to master. I am a bad programmer

---
+++ docs/precache-manifest.8f1ec5557a5321a5d3945e5ceaf92b66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43645a203026c2ded6fac464ae8e4582",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e75605e383ca8321941",
+    "url": "/static/css/main.5b0cff14.chunk.css"
+  },
+  {
+    "revision": "632bf5600a3427442972",
+    "url": "/static/js/2.7db020fd.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.7db020fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e75605e383ca8321941",
+    "url": "/static/js/main.ec41f97e.chunk.js"
+  },
+  {
+    "revision": "6ce07bb538fd07ff8ae5",
+    "url": "/static/js/runtime-main.6c8890a7.js"
+  },
+  {
+    "revision": "3ada9815c06d2619d52d16a5601c96b2",
+    "url": "/static/media/Early-GameBoy.3ada9815.ttf"
+  }
+]);
